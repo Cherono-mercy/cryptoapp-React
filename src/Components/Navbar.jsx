3856747c66adda41215913ba0d8b391c5fdc7127
mercy cherono <cheronom142@gmail.com>
2024-11-28 14:23:06 +0300

    updated navbar component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -48,8 +48,7 @@ const Navbar = () => {
                 <Menu.Item icon={ <FundOutlined />}>
                 <Link to="/cryptocurrencies" > Cryptocurrencies </Link>
                 </Menu.Item>
-                <Menu.Item icon={ <MoneyCollectOutlined />}>
-                </Menu.Item>
+                
                 <Menu.Item icon={ <BulbOutlined />}>
                 <Link to="/news" > News </Link>
                 </Menu.Item>
